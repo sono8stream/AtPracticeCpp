b997b55dce1322a508c26e47233854d0a04f2a3b
SONO <sono.stream8580@gmail.com>
2023-12-10 22:02:03 +0900

    Fix Rolling Hash

--- Library/RollingHash.cpp
+++ Library/RollingHash.cpp
@@ -10,26 +10,26 @@ namespace ABC {
 		private:
 			static const int BASE_COUNT = 5;
 
-			static const long* getMods() {
+			static const int* getMods() {
 				// 基底は以下をとる。1つの基底につき、ハッシュ値の衝突確率は(1-(N-1))/p（Nは対象の文字列の長さ。pは基底のサイズ）らしい。要勉強。
-				static const long mods[BASE_COUNT] = { 998244353, 1000000007, 1000000009, 1000000021, 1000000033 };
+				static const int mods[BASE_COUNT] = { 998244353, 1000000007, 1000000009, 1000000021, 1000000033 };
 				return mods;
 			}
 
-			static const long* getBases() {
+			static const int* getBases() {
 				// 念のためアルファベットの個数よりは大きな値としておく。なんでこの数で良いのかは追って勉強。
-				static const long mods[BASE_COUNT] = { 31, 37, 41, 43, 47 };
-				return mods;
+				static const int bases[BASE_COUNT] = { 31, 37, 41, 43, 47 };
+				return bases;
 			}
 
 		public:
 
-			long leftHashs[BASE_COUNT];// ハッシュ値
-			long pows[BASE_COUNT];// 桁数に応じたべき乗を保持しておく。
+			int hashs[BASE_COUNT];// ハッシュ値
+			int pows[BASE_COUNT];// 桁数に応じたべき乗を保持しておく。
 
 			RollingHash() {
 				for (int i = 0; i < BASE_COUNT; i++) {
-					this->leftHashs[i] = 0;
+					this->hashs[i] = 0;
 					this->pows[i] = 1;
 				}
 			}
@@ -37,19 +37,18 @@ namespace ABC {
 			// コピーコンストラクタ
 			RollingHash(const RollingHash& another) {
 				for (int i = 0; i < BASE_COUNT; i++) {
-					this->leftHashs[i] = another.leftHashs[i];
+					this->hashs[i] = another.hashs[i];
 					this->pows[i] = another.pows[i];
 				}
 			}
 
 			RollingHash(int c) {
-				const long* mods = getMods();
-				const long* bases = getBases();
+				const int* mods = getMods();
+				const int* bases = getBases();
 
 				// 1文字の場合のハッシュを取っておく。
 				for (int i = 0; i < BASE_COUNT; i++) {
-					this->leftHashs[i] = c % mods[i];
-
+					this->hashs[i] = c % mods[i];
 					this->pows[i] = bases[i];
 				}
 			}
@@ -58,18 +57,19 @@ namespace ABC {
 			bool operator==(const RollingHash& another) const {
 				bool res = true;
 				for (int i = 0; i < BASE_COUNT; i++) {
-					res &= this->leftHashs[i] == another.leftHashs[i];
+					res &= this->hashs[i] == another.hashs[i];
 				}
 				return res;
 			}
 
 			static void integrate(const RollingHash& left, const RollingHash& right, RollingHash& dst) {
-				const long* mods = getMods();
-				const long* bases = getBases();
+				const int* mods = getMods();
 
 				for (int i = 0; i < BASE_COUNT; i++) {
-					dst.leftHashs[i] = (left.leftHashs[i] + right.leftHashs[i] * left.pows[i]) % mods[i];
-					dst.pows[i] = left.pows[i] * right.pows[i] % mods[i];
+					// オーバーフローしないよう、最初に計算する値をlongにキャストしておく。
+					// 最初のものでなければオーバーフローする。最初にオーバーフローしやすい掛け算の頭に1LLをつけておくのが良いだろう。
+					dst.hashs[i] = (left.hashs[i] + 1LL * right.hashs[i] * left.pows[i]) % mods[i];
+					dst.pows[i] = 1LL * left.pows[i] * right.pows[i] % mods[i];
 				}
 			}
 		};
